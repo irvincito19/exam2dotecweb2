6715d9468f9153a1bd9007192f4cfc3ea95beba1
Sandra Garcia <isabel.garccs@gmail.com>
2025-05-08 16:50:51 -0600

    Evidencia del examen

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1. Selecciona el elemento con id "precio" y guárdalo en una variable llamada `precioElemento`.
+const precioElemento = document.getElementById("precio");
+
+// 2. Selecciona el input de cantidad por su id y guárdalo en `cantidadInput`.
+const cantidadInput = document.getElementById("cantidad");
+
+// 3. Selecciona el botón de calcular por su id.
+const botonCalcular = document.getElementById("calcular");
+
+// 4. Añade un event listener al botón para escuchar el evento 'click'.
+botonCalcular.addEventListener('click', () => {
+    // 5. Obtén el valor numérico del precio base (textContent del span).
+    const precioBase = parseInt(precioElemento.textContent);
+
+    // 6. Obtén el valor del input de cantidad (usa .value).
+    const cantidad = parseInt(cantidadInput.value);
+
+    // 7. Calcula el total (precioBase * cantidad).
+    const total = precioBase * cantidad;
+
+    // 8. Muestra el resultado en la consola.
+    console.log(total);
+
+    // 9. Actualiza el texto del elemento con id "resultado" con el mensaje: "Total a pagar: $X".
+    document.getElementById("resultado").textContent = "Total a pagar: $" + total;
+});
---
+++ app.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const precioBase = ref(200); // Precio base
+const cantidad = ref(1); // Cantidad ingresada por el usuario
+const total = ref(0);
+
+const calcularTotal = () => {
+  total.value = precioBase.value * cantidad.value;
+}
+</script>
+
+<template>
+  <HelloWorld msg="Examen 2do parcial - Tecnologías Web I" />
+  <div>
+    <p>Precio: $<span>{{ precioBase }}</span></p>
+    <input type="number" v-model="cantidad" placeholder="Cantidad">
+    <button @click="calcularTotal">Calcular</button>
+    <p id="resultado">Total a pagar: ${{ total }}</p>
+  </div>
+
+  <p>
+    Recuerda trabajar en una rama (Ejemplo: irvinghdez_exam2branch_VUE) y subir solo un cambio.
+  </p>
+  <p class="read-the-docs">"El pensamiento tecnológico es la capacidad de ver el mundo a través de la lente de la innovación y la creatividad."</p>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
